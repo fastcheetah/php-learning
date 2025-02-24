4e5930c5829f21dd4ced9a3aeabdd2073ce34f0e
Kelvin <edeh47245@gmail.com>
2025-02-24 22:57:29 +0100

    Making our own simple  max maths function

--- site.php
+++ site.php
@@ -100,6 +100,20 @@
             echo "You are female";
            }
            ?>
+          <?php 
+          function getMax($num1, $num2, $num3){
+            if($num1 >= $num2 && $num1 >= $num3){
+                return $num1;
+            }elseif($num2 >= $num1 && $num2 >= $num3){
+                return $num2;
+            }else{
+                return $num3;
+            }
+          };
+          echo getMax(300, 90, 100);
+          ?>
+         
+
             
 </body>
 </html>
