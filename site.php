99fe48e4721ef8ae041216416552270d184df0ae
Kelvin <edeh47245@gmail.com>
2025-02-24 23:21:07 +0100

    Building a grade system with switch statements

--- site.php
+++ site.php
@@ -138,5 +138,32 @@
 
             ?>
 
+            <form action="site.php" method = "post">
+              What was your grade?
+            <input type="text" name="grade">
+            <input type="submit">
+            </form>
+<?php 
+$grade = $_POST["grade"];
+switch($grade){
+    case "A":
+        echo "You did amazing!";
+        break;
+    case "B":
+        echo "You did pretty good!";
+        break;
+    case "C":
+        echo "You did poorly!";
+        break;
+    case "D":
+        echo "You did very bad!";
+        break;
+    case "F":
+        echo "You failed!";
+        break;
+    default:
+        echo "Invalid grade";
+}
+?>
 </body>
 </html>
