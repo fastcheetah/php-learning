d8849862699e3402ad5962f3823638d154f5b397
Kelvin <edeh47245@gmail.com>
2025-02-24 23:12:30 +0100

    Building an advanced calculator with if statements php

--- site.php
+++ site.php
@@ -113,7 +113,30 @@
           echo getMax(300, 90, 100);
           ?>
          
+            <form action="site.php" method="post">
+            First Num:<input type="number" name = "num1" step = "0.001"><br>
+            op: <input type="text" name = "op"><br>
+            Second Num:<input type="number" name = "num2"><br>
+            <input type="submit">
+            </form>
+            Answer:
+            <?php 
+            $num1 = $_POST["num1"];
+            $num2 = $_POST["num2"];  
+            $op = $_POST["op"];
+            if($op == "+"){
+                echo $num1 + $num2;
+            }elseif($op == "-"){
+                echo $num1 - $num2;
+            }elseif($op == "*"){
+                echo $num1 * $num2;
+            }elseif($op == "/"){
+                echo $num1 / $num2;
+            }else{
+                echo "Invalid operator";
+            }
 
-            
+            ?>
+
 </body>
 </html>
