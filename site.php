3fb04fbb363a65e14691c6f37999d83a881a0875
Kelvin <edeh47245@gmail.com>
2025-02-26 23:16:23 +0100

    Making constructors in php

--- site.php
+++ site.php
@@ -200,18 +200,26 @@ class Book{
     var $author;
     var $pages;
 
+    function __construct($aTitle,$aAuthor,$aPages){
+        $this->title = $aTitle;
+        $this->author = $aAuthor;
+        $this->pages = $aPages;
+        
+        
+    }
+
 }
-$book1 = new Book;
+$book1 = new Book("Harry Potter", "JK Rowling", 400);
 $book1->title = "Harry Potter";
 $book1->author = "JK Rowling";
 $book1->pages = 400;
-echo $book1->title;
 
-$book2 = new Book;
+
+$book2 = new Book("Lord of the Rings");
 $book2->title = "Lord of the Rings";
 $book2->author = "Tolkein"; 
 $book2->pages = 700;
-echo $book2->title;
+
 ?>
 
 </body>
