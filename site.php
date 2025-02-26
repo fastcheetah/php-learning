0c1e0d3a7174c3ce9ca6c68c343cef4475c08041
Kelvin <edeh47245@gmail.com>
2025-02-26 23:06:03 +0100

    Making classes and objects

--- site.php
+++ site.php
@@ -193,6 +193,26 @@ include "article-header.php";
 sayHi("mike") 
 ?>
 <?php include "footer.html"?>
+<!-- //classes -->
+<?php
+class Book{
+    var $title;
+    var $author;
+    var $pages;
+
+}
+$book1 = new Book;
+$book1->title = "Harry Potter";
+$book1->author = "JK Rowling";
+$book1->pages = 400;
+echo $book1->title;
+
+$book2 = new Book;
+$book2->title = "Lord of the Rings";
+$book2->author = "Tolkein"; 
+$book2->pages = 700;
+echo $book2->title;
+?>
 
 </body>
 </html>
