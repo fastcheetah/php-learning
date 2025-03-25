40e984976ebd0a4ca596e10175ac25970e131aea
Kelvin <edeh47245@gmail.com>
2025-03-25 18:38:13 +0100

    Using define to make constants in php

---
+++ learn.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+echo "Hello page";
+//CONSTANTS
+define("GREETING", "Hello you!");
+echo GREETING;
+?>
+</body>
+</html>
